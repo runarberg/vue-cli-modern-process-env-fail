c0b3839beb77c89a9a83ce814707663b43898d11
Rúnar Berg <runar.BaugssonSigridarson@netapp.com>
2019-10-07 15:09:30 -0700

    Provide a minimal working example

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld :msg="msg"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 
+const { VUE_APP_MSG: msg } = process.env;
+
 export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  data() {
+    return { msg };
   }
 }
 </script>
